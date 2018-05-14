b467d29828cd88021326103ccccd258035c1b3dc
operatorequals <john.torakis@gmail.com>
2018-05-14 18:05:43 +0300

    Created the 'main' function in __main__

--- satoriremote/__main__.py
+++ satoriremote/__main__.py
@@ -4,67 +4,73 @@ import sys
 import satoriremote
 
 
+def main():
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("--remote", '-r',
-		help="The connection string to test",
-	)
+	parser = argparse.ArgumentParser()
 
-parser.add_argument("--list", '-l',
-		help="",
-		action="store_true",
-	)
+	parser.add_argument("--remote", '-r',
+			help="The connection string to test",
+		)
 
-args = parser.parse_args()
+	parser.add_argument("--list", '-l',
+			help="",
+			action="store_true",
+		)
 
-if not args.list and not args.remote:
-	parser.print_usage()
-	sys.exit(-1)
+	args = parser.parse_args()
 
-if args.list:
-	print (satoriremote.AVAILABLE_PROTOCOLS) 
+	if not args.list and not args.remote:
+		parser.print_usage()
+		sys.exit(-1)
 
-if args.remote:
-	conn, conn_dict = satoriremote.connect(args.remote)
+	if args.list:
+		print (satoriremote.AVAILABLE_PROTOCOLS) 
 
-	with conn as obj:
-		if obj:
-			print ("[+] Connection Successful!")
-		else:
-			print ("[-] Connection Failed.") 
+	if args.remote:
+		conn, conn_dict = satoriremote.connect(args.remote)
 
-		if conn_dict['path']:
-			try:
-				st_res = obj.lstat(conn_dict['path'])
-				print ("[+] 'stat' call to '{}' succeded!".
-						format(conn_dict['path'])
-					)
-				# print (st_res)
+		with conn as obj:
+			if obj:
+				print ("[+] Connection Successful!")
+			else:
+				print ("[-] Connection Failed.") 
 
-			except Exception as e:
-				# print (e)
-				print ("[-] Failed to use 'stat' on remote file '{}'".
-						format(conn_dict['path'])
-				) 
-				sys.exit(1)
-			try:
-				import stat
-				if stat.S_ISDIR(st_res.st_mode):
-					dlist = obj.listdir(conn_dict['path'])
-					print ("[+] Directory Listing for '{}' succeded!".
+			if conn_dict['path']:
+				try:
+					st_res = obj.lstat(conn_dict['path'])
+					print ("[+] 'stat' call to '{}' succeded!".
 							format(conn_dict['path'])
 						)
-					print (
-							"\tFiles under '{}':".format(
-								conn_dict['path']
+					# print (st_res)
+
+				except Exception as e:
+					# print (e)
+					print ("[-] Failed to use 'stat' on remote file '{}'".
+							format(conn_dict['path'])
+					) 
+					sys.exit(1)
+				try:
+					import stat
+					if stat.S_ISDIR(st_res.st_mode):
+						dlist = obj.listdir(conn_dict['path'])
+						print ("[+] Directory Listing for '{}' succeded!".
+								format(conn_dict['path'])
 							)
-						)
-					[ print (f) for f in dlist ]
-					
-			except Exception as e:
-				# print (type(e))
-				print ("[-] Failed to list directory: '{}'".
-						format(conn_dict['path'])
-				) 
-				sys.exit(2)
+						print (
+								"\tFiles under '{}':".format(
+									conn_dict['path']
+								)
+							)
+						[ print (f) for f in dlist ]
+						
+				except Exception as e:
+					# print (type(e))
+					print ("[-] Failed to list directory: '{}'".
+							format(conn_dict['path'])
+					) 
+					sys.exit(2)
+
+
+if __name__ == '__main__':
+	main()
